250a448cc56d7682812c0f0ad272b5f981f627f7
tonybui <tony.tung.bui@gmail.com>
2020-05-19 20:52:40 -0700

    update job correctly

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ "`git status -s`" ]
+if [[ $(git status -s) ]]
 then
     echo "The working directory is dirty. Please commit any pending changes."
     exit 1;
@@ -13,16 +13,24 @@ git worktree prune
 rm -rf .git/worktrees/public/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public upstream/gh-pages
+git worktree add -B gh-pages public origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
 hugo
+echo "info.tbui.net" > public/CNAME
 
 echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+read -erp "Commit Message: " commitMessage
+cd public && git add --all && git commit -m "[publish gh-pages] $commitMessage"
 
-#echo "Pushing to github"
-#git push --all
+read -erp "Push gh-pages to origin? (y/n): " doPushToRemote
+
+if [[ "$doPushToRemote" == "y" ]]
+then
+    echo "Pushing to remote..."
+    git push origin gh-pages
+fi
+
